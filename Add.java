eaac7a6ded4f1bd7b0777e75b1836436c855ba6f
sijypaul <104199522+sijypaul@users.noreply.github.com>
2022-05-04 15:07:43 +0530

    added

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Add
+{
+		 public static void main(String[] args) 
+		 {
+				Scanner sc=new Scanner(System.in);
+				System.out.print("Enter the numbers::");
+				int a=sc.nextInt();
+				int b=sc.nextInt();
+				int c=a+b;
+				System.out.print("c="+c);
+
+		  }
+
+}
