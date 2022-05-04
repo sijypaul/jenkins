7bddce41cc2416ac10229c8173abeba2c2830ccb
sijypaul <104199522+sijypaul@users.noreply.github.com>
2022-05-04 14:44:29 +0530

    uploaded

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.Scanner;
+public class Calculator
+{
+	public static void main(String[] args) throws Exception
+	{
+		int a=20;
+		int b=2;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the operators: (+,-,*,/)");
+		String optr=sc.next();
+		if(optr.equals("+"))
+		{
+		System.out.println("Sum: "+(a+b));
+		}
+		else if(optr.equals("-"))
+		{
+		System.out.println("Difference: "+(a-b));
+		}
+		else if(optr.equals("*"))
+		{
+		System.out.println("Product: "+(a*b));
+		}
+		else if(optr.equals("/"))
+		{
+		System.out.println("Quotient: "+(a/b));
+		}
+		else
+		{
+		System.out.println("Invalid operator......");
+		}
+	}
+}
