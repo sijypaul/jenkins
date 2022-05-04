aebca12a97038ebfe53c9682790ac3893c74d5e7
sijypaul <104199522+sijypaul@users.noreply.github.com>
2022-05-04 15:34:11 +0530

    uplodedd

--- Calc.java
+++ Calc.java
@@ -5,9 +5,9 @@ public class Calc
 	public static void main(String[] args) throws Exception
 	{
 		//System.out.print("Enter the operators: (+,-,*,/)");
-		int a=Integer.parseInt(args[0]);
-		int b=Integer.parseInt(args[1]);
-		String optr=args[2];
+		String optr=args[0];
+		int a=Integer.parseInt(args[1]);
+		int b=Integer.parseInt(args[2]);
 
 		if(optr.equals("+"))
 		{
